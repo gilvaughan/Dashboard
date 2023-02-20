2c497f67de64c9dcbbbacd79a9ffdf81fe9b5dbb
gilvaughan <125915773+gilvaughan@users.noreply.github.com>
2023-02-20 15:04:00 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -166,3 +166,15 @@ if selection_buttons == 'Buble Graph':
     )
 
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+
+#---- HIDE STREAMLIT STYLE ----
+
+hide_st_style = """
+            <style>
+            MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)    
